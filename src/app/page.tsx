35a9ef389e8e88ea8f59f259575225d2f28801ce
Alexander Curtin <acurtin@xtivia.com>
2024-01-22 18:40:06 -0500

    set up home page to work with PageHeader and Hero, add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 import { fetchLandingEntriesBySlug } from '~/api'
 
 import PageHeader from '~/components/PageHeader';
+import Hero from '~/components/Hero';
 
 export default async function Home() {
   const [data]: any = await fetchLandingEntriesBySlug();
 
   return (
-    <main>
-      <h1>{data.fields.pageTitle}</h1>
+    <main className='w-full flex flex-col items-center'>
       {data.fields.sections.map((section: any) => {
         switch(section.fields.internalName) {
           case ('PageHeader'): {
-            return <PageHeader />
+            return <PageHeader data={section} />
+          }
+          case ('HomeHero'): {
+            return <Hero data={section} />
           }
           default:
             return <div>{section.fields.internalName}</div>
