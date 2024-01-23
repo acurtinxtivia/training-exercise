bac814cfcf6449e175273a55999dd60729fd727b
Alexander Curtin <acurtin@xtivia.com>
2024-01-23 14:01:13 -0500

    add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import type { PropsWithChildren } from "react";
+
+interface ButtonProps {
+    theme: 'primary' | 'secondary' | 'image' | undefined,
+}
+
+const Button = ({ theme = 'primary', children }: PropsWithChildren<ButtonProps>) => {
+    return (
+        <button className={`btn ${theme === 'primary' ? 'btn-primary' : 'btn-secondary'}`}>
+            {children}
+        </button>
+    )
+}
+
+export default Button;
