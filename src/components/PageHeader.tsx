9f2f4fed292c7a1a13021f839661e9a7ee930a97
Alexander Curtin <acurtin@xtivia.com>
2024-01-24 09:16:24 -0500

    add separate mobile and desktop navbars

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,30 +1,44 @@
-import Link from "next/link"
+'use client'
+import { useState } from 'react'
 
 import { TypeHeaderFields } from "../../types/contentful"
 import ImageWithFocalPoint from "./ImageWithFocalPoint"
+import NavBar from "./NavBar"
+import MobileNav from './MobileNav'
+import BarsIcon from './icons/BarsIcon'
+import ArrowLeftIcon from './icons/ArrowLeftIcon'
 
 const PageHeader = ({data}: { data: { fields: TypeHeaderFields } }) => {
+    const [navOpen, setNavOpen] = useState(false)
     const logo = data.fields.logo
-    const navigationItems = data.fields.navigationMenu?.fields.navigationItems || []
 
     return (
-        <header className="w-full ">
+        <header className="w-full">
             <div className="w-full flex justify-center">
-                <div className={"w-full flex justify-between py-6"} style={{ maxWidth: data.fields.maxWidth}}>
-                    <ImageWithFocalPoint fields={logo.fields} />
+                <div className="w-full flex justify-between p-6" style={{ maxWidth: data.fields.maxWidth}}>
+                    <div className="flex gap-4">
+                        <button className="md:hidden" onClick={() => setNavOpen(!navOpen)}>
+                            {navOpen 
+                                ? (<ArrowLeftIcon className='h-10 w-10'/>) 
+                                : (<BarsIcon className='h-10 w-10'/>)
+                            }
+                        </button>
+                        <ImageWithFocalPoint fields={logo.fields} />
+                    </div>
                 </div>
             </div>
-            <nav className="w-full bg-primary-dark flex justify-center py-4 text-white">
-                <ul className={"w-full flex gap-8 font-bold"} style={{ maxWidth: data.fields.maxWidth }}>
-                    {navigationItems.map(item => (
-                        <ul key={item.sys.id}>
-                            <Link href={`/${item.fields.slug}`}>
-                                {item.fields.label}
-                            </Link>
-                        </ul>
-                    ))}
-                </ul>
-            </nav>
+            <div className='relative'>
+            {navOpen && (
+                <MobileNav
+                    fields={data.fields.navigationMenu?.fields}
+                    maxWidth={data.fields.maxWidth}
+                />
+            )}
+            <NavBar 
+                fields={data.fields.navigationMenu?.fields}
+                maxWidth={data.fields.maxWidth}
+                />
+            </div>
         </header>
     )
 }
