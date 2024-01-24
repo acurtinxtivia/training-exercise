a2c718d12cf4e4a337ba2c4bc6250a52471ffd16
Alexander Curtin <acurtin@xtivia.com>
2024-01-24 09:23:56 -0500

    update to handle more components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { fetchLandingEntriesBySlug } from '~/api'
 import PageHeader from '~/components/PageHeader';
 import Hero from '~/components/Hero';
 import SetOfCards from '~/components/SetOfCards';
+import SetOfTestimonials from '~/components/SetOfTestimonials';
+import Banner from '~/components/Banner';
+import Footer from '~/components/Footer';
 
 export default async function Home() {
   const [data]: any = await fetchLandingEntriesBySlug();
@@ -11,11 +14,17 @@ export default async function Home() {
       {data.fields.sections.map((section: any) => {
         switch(section.sys.contentType.sys.id) {
           case 'header':
-            return <PageHeader data={section} />
+            return <PageHeader data={section} key={section.sys.id} />
           case 'heroImage':
-            return <Hero data={section} />
+            return <Hero data={section} key={section.sys.id} />
+          case 'banner':
+            return <Banner fields={section.fields} key={section.sys.id} />
           case 'setOfCard':
-            return <SetOfCards data={section} />
+            return <SetOfCards data={section} key={section.sys.id} />
+          case 'setOfTestimonials':
+            return <SetOfTestimonials data={section} key={section.sys.id} />
+          case 'footer':
+            return <Footer data={section} />
           default:
             return null
         }
