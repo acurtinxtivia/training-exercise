8ea7bdcb4b27ef510fce507bd665087680fbbfc5
Alexander Curtin <acurtin@xtivia.com>
2024-01-24 09:10:34 -0500

    edit styling

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -13,9 +13,9 @@ const Heading = ({ size, children, color = '', className = '' }: PropsWithChildr
         case 'h1':
             return <h1 className={cn('text-5xl', colorClass, className)}>{children}</h1>
         case 'h2':
-            return <h2 className={cn('text-xl', colorClass, className)}>{children}</h2>
+            return <h2 className={cn('text-3xl', colorClass, className)}>{children}</h2>
         case 'h3':
-            return <h3 className={cn('text-lg', colorClass, className)}>{children}</h3>
+            return <h3 className={cn('text-xl', colorClass, className)}>{children}</h3>
         case 'h4':
             return <h4>{children}</h4>
         case 'h5':
