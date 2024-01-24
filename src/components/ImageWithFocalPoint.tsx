f6e45623f7f38704114d14736038d211686a42ae
Alexander Curtin <acurtin@xtivia.com>
2024-01-24 11:09:39 -0500

    fix image url

--- src/components/ImageWithFocalPoint.tsx
+++ src/components/ImageWithFocalPoint.tsx
@@ -5,7 +5,7 @@ import { TypeImageWithFocalPointFields } from "../../types/contentful";
 const ImageWithFocalPoint = ({ fields, className = '' }: { fields: TypeImageWithFocalPointFields, className?: string }) => {
     return (
         <Image
-            src={`http:${fields.image.fields.file?.url}`}
+            src={`https:${fields.image.fields.file?.url}`}
             alt={fields.altText}
             height={fields.image.fields.file?.details.image.height}
             width={fields.image.fields.file?.details.image.width}
