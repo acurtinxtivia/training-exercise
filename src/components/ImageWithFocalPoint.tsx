021c89abc0cf86356365aa0d0cbaffb2d0fefb66
Alexander Curtin <acurtin@xtivia.com>
2024-01-24 09:11:44 -0500

    add className prop

--- src/components/ImageWithFocalPoint.tsx
+++ src/components/ImageWithFocalPoint.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 import { TypeImageWithFocalPointFields } from "../../types/contentful";
 
-const ImageWithFocalPoint = ({ fields }: { fields: TypeImageWithFocalPointFields }) => {
+const ImageWithFocalPoint = ({ fields, className = '' }: { fields: TypeImageWithFocalPointFields, className?: string }) => {
     return (
         <Image
             src={`http:${fields.image.fields.file?.url}`}
             alt={fields.altText}
             height={fields.image.fields.file?.details.image.height}
             width={fields.image.fields.file?.details.image.width}
+            className={className}
         />
     )
 }
