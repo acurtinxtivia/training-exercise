87ab39d0e5118be9c82b3accf6aa92b0a60d0026
Alexander Curtin <acurtin@xtivia.com>
2024-01-25 09:28:28 -0500

    edit styling

--- src/components/SetOfCards.tsx
+++ src/components/SetOfCards.tsx
@@ -16,7 +16,7 @@ const SetOfCards = ({ fields }: { fields: TypeSetOfCardFields }) => {
                 {fields.title && (
                     <Heading size={fields.titleSize} className='font-black pb-16'>{fields.title}</Heading>
                 )}
-                <div className={cn(cardDisplayClass[fields.cardDisplayStyle], 'w-full p-4 flex flex-col gap-12')}>
+                <div className={cn(cardDisplayClass[fields.cardDisplayStyle], 'w-full flex flex-col gap-12')}>
                     {fields.cards.map((card) => (
                         <Card key={card.sys.id} data={card} />
                     ))}
