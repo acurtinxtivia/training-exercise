2f9dece36b6e5c8322d4c3ac8d8db52704932ba1
Alexander Curtin <acurtin@xtivia.com>
2024-01-26 18:02:38 -0500

    add icons

--- src/components/icons/ChevronRightIcon.tsx
+++ src/components/icons/ChevronRightIcon.tsx
@@ -1,7 +1,7 @@
-const ChevronRightIcon = ({ className = "w-6 h-6" }: { className?: string }) => {
+const ChevronRightIcon = ({ className = "w-6 h-6", strokeWidth = 0.5 }: { className?: string, strokeWidth?: number }) => {
 
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={0.5} stroke="currentColor" className={className}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={strokeWidth} stroke="currentColor" className={className}>
             <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
         </svg>
     )
