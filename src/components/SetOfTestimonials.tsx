61e0bada2e8edfadb6caf20d5f9eda4f758afb8b
Alexander Curtin <acurtin@xtivia.com>
2024-01-26 15:45:56 -0500

    add content & styling to testimonials

--- src/components/SetOfTestimonials.tsx
+++ src/components/SetOfTestimonials.tsx
@@ -18,15 +18,25 @@ const SetOfTestimonials = ({ fields }: { fields: TypeSetOfTestimonialsFields }) 
     return (
         <section className="w-full flex justify-center pt-[68px]">
             <div className="w-full flex flex-col items-center" style={{ maxWidth: fields.maxWidth }}>
-                <Heading size={fields.titleSize} className='font-black pb-16'>{fields.title}</Heading>
-                <div className='w-full flex items-center gap-2'>
-                    <button onClick={() => scroll(-250)}><ChevronLeftIcon className='h-20 w-20 text-gray-400' /></button>
-                    <div className="w-full flex flex-no-wrap overflow-x-scroll hide-scrollbar scroll-smooth gap-12" ref={scrollArea}>
+                <Heading 
+                    size={fields.titleSize} 
+                    className='font-black pb-[26px] text-[28px] md:text-[36px] lg:text-[46px]'
+                >
+                    {fields.title}
+                </Heading>
+                <div className='w-[50px] lg:w-[68px] h-[3px] bg-primary mb-[40px] md:mb-[60px]' />
+                <div className='w-full flex items-center gap-2 mx-auto max-w-[480px] sm:max-w-none sm:mx-none'>
+                    <button onClick={() => scroll(-250)} className='group hidden sm:block'>
+                        <ChevronLeftIcon className='w-20 h-20 text-silver group-active:text-primary' strokeWidth={0.4} />
+                    </button>
+                    <div className="w-full flex flex-no-wrap overflow-x-scroll hide-scrollbar scroll-smooth gap-[66px]" ref={scrollArea}>
                         {fields.testimonials?.map((testimonial) => (
                             <Testimonial key={testimonial.sys.id} fields={testimonial.fields} />
                             ))}
                     </div>
-                    <button onClick={() => scroll(250)}><ChevronRightIcon className='h-20 w-20 text-gray-400' /></button>
+                    <button onClick={() => scroll(250)} className='group hidden sm:block'>
+                        <ChevronRightIcon className='h-20 w-20 stroke-silver group-active:stroke-primary' strokeWidth={0.4} />
+                    </button>
                 </div>
             </div>
         </section>
--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,16 +1,24 @@
 import { documentToReactComponents} from '@contentful/rich-text-react-renderer'
 
 import { TypeTestimonialFields } from "../../types/contentful"
+import ImageWithFocalPoint from './ImageWithFocalPoint'
 
 const Testimonial = ({ fields }: { fields: TypeTestimonialFields }) => {
     return (
-        <div className="min-w-[300px] flex flex-col items-center justify-between shadow-md border px-8 py-12 rounded-md text-center gap-4">
-            <em className='text-light-gray'>
+        <div 
+            className="min-w-[308px] flex flex-col items-center justify-between shadow-light border border-off-white/70 px-[30px] pt-[40px] pb-[30px] rounded-[5px] text-center gap-[22px]"
+        >
+            <div className='w-full flex justify-around items-center gap-4'>
+                <div className='border-b scale-y-[0.3] border-light-gray/30 w-full' />
+                <ImageWithFocalPoint fields={fields.image.fields} className='h-20 w-20 object-cover rounded-full'/>
+                <div className='border-b scale-y-[0.3] border-light-gray/30 w-full' />
+            </div>
+            <em className='text-light-gray font-light leading=[21px]'>
                 {documentToReactComponents(fields.testimonial)}
             </em>
             <div>
                 <strong>{fields.name}</strong>
-                <p className='text-extra-light-gray'>{`(${fields.location})`}</p>
+                <p className='text-extra-light-gray'>{`(${fields.position})`}</p>
             </div>
         </div>
     )
