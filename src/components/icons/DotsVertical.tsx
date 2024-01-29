9528568ea0054ee54ec9b89878a10409dd6597d1
Alexander Curtin <acurtin@xtivia.com>
2024-01-29 15:18:49 -0500

    add social icons

--- src/components/icons/DotsVertical.tsx
+++ src/components/icons/DotsVertical.tsx
@@ -1,9 +1,6 @@
 const DotsVertical = ({ className = 'w-6 h-6' }: { className?: string }) => {
 
     return (
-        // <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        //     <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-        // </svg>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
         </svg>
