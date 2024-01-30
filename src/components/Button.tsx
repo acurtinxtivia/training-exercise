c5ee558363d84a898024d6a978dd5943204a807c
Alexander Curtin <acurtin@xtivia.com>
2024-01-30 10:48:42 -0500

    add button style variation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,14 @@ import type { PropsWithChildren } from "react";
 import cn from 'classnames'
 
 interface ButtonProps {
-    theme?: 'primary' | 'secondary' | 'white' | 'image' | undefined;
+    theme?: 'primary' | 'primary outline' | 'secondary' | 'white' | 'image' | undefined;
     className?: string;
 }
 
 const Button = ({ theme = 'primary', className = '', children }: PropsWithChildren<ButtonProps>) => {
     const classNames = {
         'primary': 'btn-primary',
+        'primary outline': 'btn-primary-outline',
         'secondary': 'btn-secondary',
         'white': 'btn-white',
         'image': ''
