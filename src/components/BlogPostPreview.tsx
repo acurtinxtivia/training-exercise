132c4b9ede199a38e02d5f5655a05d8041108f38
Alexander Curtin <acurtin@xtivia.com>
2024-01-30 17:41:26 -0500

    add styling

--- src/components/BlogPostPreview.tsx
+++ src/components/BlogPostPreview.tsx
@@ -1,42 +1,22 @@
-'use client'
 import Link from "next/link"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
-import Heading from "./Heading"
 import Button from "./Button"
-import ImageWithFocalPoint from "./ImageWithFocalPoint"
 import BlogPostHeader from "./BlogPostHeader"
 
 const BlogPostPreview = ({ fields, createdAt }: { fields: any, createdAt: string }) => {
-    console.log(fields)
-
     return (
         <div>
             {fields ? (
-                <div className="flex flex-col p-4">
-                    {/* <div className="flex-shrink-0">
-                        <ImageWithFocalPoint fields={fields.featuredImage.fields} className="w-full rounded-sm"/>
+                <div className="flex flex-col sm:px-4">
+                    <BlogPostHeader fields={fields} createdAt={createdAt} />
+                    <div className="w-full max-h-[156px] overflow-hidden mt-[18px] text-light-gray font-light leading-[26px]">
+                        {documentToReactComponents(fields.postContent)}
                     </div>
-                    <div className="flex flex-col mt-[22px]">
-                        <Heading size='h3' className="font-black">{fields.title}</Heading>
-                        <div className="flex text-light-gray gap-[22px] items-center mt-[8px]">
-                            <p className=""><span className="font-bold text-black">Date:</span> {new Date(createdAt).toLocaleDateString('en-US', {
-                               year: 'numeric',
-                               month: 'long',
-                               day: 'numeric'
-                            })}</p>
-                            <div className="h-[18px] border-l"></div>
-                            <p className=""><span className="font-bold text-black">Category:</span> {fields.topic}</p>
-                        </div>
-                        <hr className="mt-[18px]" /> */}
-                        <BlogPostHeader fields={fields} createdAt={createdAt} />
-                        <div className="text-sm w-full max-h-24 overflow-hidden mt-[18px]">
-                            {documentToReactComponents(fields.postContent)}
-                        </div>
-                        <Link href={`/blog/${fields.slug}`} className="mt-[29px]">
-                            <Button theme="primary outline" className="text-md">Read More</Button>
-                        </Link>
-                    </div>
+                    <Link href={`/blog/${fields.slug}`} className="mt-[29px]">
+                        <Button theme="primary outline" className="text-md">Read More</Button>
+                    </Link>
+                </div>
             ) : (
                 <div>not availaible</div>
             )}
