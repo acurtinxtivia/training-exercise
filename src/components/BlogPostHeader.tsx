075ae9e50f91e312c29479d174a4aad3dad1094c
Alexander Curtin <acurtin@xtivia.com>
2024-01-30 17:41:57 -0500

    edit styling

--- src/components/BlogPostHeader.tsx
+++ src/components/BlogPostHeader.tsx
@@ -1,33 +1,24 @@
-import Link from "next/link"
-
 import ImageWithFocalPoint from "./ImageWithFocalPoint"
 import Heading from "./Heading"
-import Button from "./Button"
 
 const BlogPostHeader = ({ fields, createdAt }: { fields: any, createdAt: string }) => {
     return (
-        <div className="flex flex-col gap-4">
-            <div className="flex-shrink-0">
-                <ImageWithFocalPoint fields={fields.featuredImage.fields} className="w-full rounded-sm"/>
+        <div className="w-full flex flex-col gap-4">
+            <div className="flex-shrink-0 ">
+                <ImageWithFocalPoint fields={fields.featuredImage.fields} className="w-full aspect-[2.11/1] h-auto object-cover rounded-sm"/>
             </div>
             <div className="flex flex-col mt-[22px]">
-                <Heading size='h3' className="font-black">{fields.title}</Heading>
-                <div className="flex text-light-gray gap-[22px] items-center mt-[8px]">
+                <Heading size='h3' className="font-black text-[22px] leading-[26px] lg:text-[26px] lg:leading-[31px] xl:text-[30px] xl:leading-[38px]">{fields.title}</Heading>
+                <div className="flex flex-col md:flex-row items-start md:items-center text-light-gray leading-[24px] gap-[3px] lg:gap-[22px] mt-[8px]">
                     <p className=""><span className="font-bold text-black">Date:</span> {new Date(createdAt).toLocaleDateString('en-US', {
                         year: 'numeric',
                         month: 'long',
                         day: 'numeric'
                     })}</p>
-                    <div className="h-[18px] border-l"></div>
+                    <div className="h-[18px] border-l hidden md:block"></div>
                     <p className=""><span className="font-bold text-black">Category:</span> {fields.topic}</p>
                 </div>
                 <hr className="mt-[18px]" />
-                {/* <div className="text-sm w-full max-h-24 overflow-hidden mt-[18px]">
-                    {documentToReactComponents(fields.postContent)}
-                </div>
-                <Link href={`/blog/${fields.slug}`} className="mt-[29px]">
-                    <Button theme="primary outline" className="text-md">Read More</Button>
-                </Link> */}
             </div>
         </div>
     )
