9133211a86d79011dcf40cb6219dfbe81f35c9ef
Alexander Curtin <acurtin@xtivia.com>
2024-01-31 11:10:46 -0500

    styling tweaks

--- src/components/ImageAndText.tsx
+++ src/components/ImageAndText.tsx
@@ -8,16 +8,23 @@ import Heading from './Heading'
 const imagePositionClass = {
     Top: 'flex-col',
     Left: 'flex-col lg:flex-row',
-    Right: 'flex-col md:flex-row-reverse',
+    Right: 'flex-col lg:flex-row-reverse',
     Bottom: 'flex-col-reverse'
+}
+
+const imageSizeClass = {
+    Top: 'w-full',
+    Left: 'lg:w-[50%]',
+    Right: 'lg:w-[50%]',
+    Bottom: 'w-full'
 }
 
 const ImageAndText = ({ fields }: { fields: TypeImageAndTextFields }) => {
     return (
         <section className="w-full flex justify-center pt-[68px]">
-            <div className={cn('px-4 flex gap-8', imagePositionClass[fields.imagePosition])} style={{ maxWidth: fields.maxWidth }}>
-                <div className='flex-shrink-0'>
-                    <ImageWithFocalPoint fields={fields.image.fields} className='rounded-md' />
+            <div className={cn('px-4 flex justify-center gap-8 sm:w-[480px] md:w-[750px] lg:w-[970px] xl:w-full', imagePositionClass[fields.imagePosition])} style={{ maxWidth: fields.maxWidth }}>
+                <div className={cn('flex-shrink-0', imageSizeClass[fields.imagePosition])}>
+                    <ImageWithFocalPoint fields={fields.image.fields} className='w-full h-full max-h-[450px] object-cover' />
                 </div>
                 <div className='flex flex-col gap-2 flex-grow-0'>
                     {fields.title && (
--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -10,13 +10,13 @@ const Testimonial = ({ fields }: { fields: TypeTestimonialFields }) => {
             className="sm:max-w-[390px] md:max-w-[319px] lg:max-w-[286px] xl:max-w-[308px] mx-[10px] md:mr-[30px] md:ml-0 lg:mr-[15px] lg:ml-0 xl:mr-[66px] xl:ml-0"
             style={{ width: 'calc(100vw - 90px)' }}
         >
-            <div className='flex flex-col items-center justify-between shadow-light border border-off-white/70 px-[30px] pt-[40px] pb-[30px] rounded-[5px] text-center gap-[22px]'>
-                <div className='w-full flex justify-around items-center gap-4'>
-                    <div className='border-b scale-y-[0.3] border-light-gray/30 w-full' />
+            <div className='flex flex-col items-center justify-between shadow-light border border-off-white/70 px-[5px] pt-[40px] pb-[30px] rounded-[5px] text-center gap-[22px]'>
+                <div className='w-full flex justify-center items-center gap-[20px]'>
+                    <div className='border-b scale-y-[0.3] border-light-gray/30 w-[66px]' />
                     <ImageWithFocalPoint fields={fields.image.fields} className='h-20 w-20 object-cover rounded-full'/>
-                    <div className='border-b scale-y-[0.3] border-light-gray/30 w-full' />
+                    <div className='border-b scale-y-[0.3] border-light-gray/30 w-[66px]' />
                 </div>
-                <em className='text-light-gray font-light leading=[21px]'>
+                <em className='text-light-gray font-light leading=[21px] px-[30px]'>
                     {documentToReactComponents(fields.testimonial)}
                 </em>
                 <div>
